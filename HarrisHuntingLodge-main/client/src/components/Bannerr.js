4da7a861315c46fdad4dc1fa1e7ce92ae824772b
Rob Harris <rob.harris.rel@gmail.com>
2023-03-30 00:19:56 -0500

    Updated Mobile View

--- HarrisHuntingLodge-main/client/src/components/Bannerr.js
+++ HarrisHuntingLodge-main/client/src/components/Bannerr.js
@@ -1,17 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import styles from './Banner.module.css';
-import { Link } from '@reach/router';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import styles from "./Banner.module.css";
+import { Link } from "@reach/router";
+import axios from "axios";
+import { FaBars } from "react-icons/fa";
 
 function Bannerr({ lodgeLogo }) {
-  const [weatherIconSrc, setWeatherIconSrc] = useState('');
-  const [temperature, setTemperature] = useState('');
-  const [currentTime, setCurrentTime] = useState('');
+  const [weatherIconSrc, setWeatherIconSrc] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [currentTime, setCurrentTime] = useState("");
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  const toggleMobileNav = () => {
+    setMobileNavOpen(!mobileNavOpen);
+  };
 
   const fetchWeather = async () => {
-    const apiKey = '5d266d214cee8ca2482175486a3d6eec';
-    const city = 'Licking';
-    const state = 'MO';
+    const apiKey = "5d266d214cee8ca2482175486a3d6eec";
+    const city = "Licking";
+    const state = "MO";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},US&appid=${apiKey}&units=imperial`;
 
     try {
@@ -21,9 +27,23 @@ function Bannerr({ lodgeLogo }) {
       setWeatherIconSrc(`https://openweathermap.org/img/wn/${iconCode}.png`);
       setTemperature(`${Math.round(weatherData.main.temp)}°F`);
     } catch (error) {
-      console.error('Error fetching weather data:', error);
+      console.error("Error fetching weather data:", error);
     }
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.target.closest(`.${styles.mobileNav}`) && !event.target.closest(`.${styles.mobileMenuIcon}`)) {
+        setMobileNavOpen(false);
+      }
+    };
+  
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+  
 
   useEffect(() => {
     fetchWeather();
@@ -35,30 +55,74 @@ function Bannerr({ lodgeLogo }) {
   }, []);
 
   return (
-    <div className={styles.headerContainer}>
-      {lodgeLogo && <img className={styles.logo} src={lodgeLogo} alt="Harris Hunting Lodge logo" />}
-      <div className={styles.titleAndLocation}>
-        <h1 className={styles.title}>Harris Hunting</h1>
-        <p className={styles.location}>Licking, MO</p>
+    <div>
+      <div className={styles.headerContainer}>
+        <div className={styles.mobileMenuIcon} onClick={toggleMobileNav}>
+          <FaBars />
+        </div>
+        {lodgeLogo && (
+          <img
+            className={styles.logo}
+            src={lodgeLogo}
+            alt="Harris Hunting Lodge logo"
+          />
+        )}
+        <div className={styles.titleAndLocation}>
+          <h1 className={styles.title}>Harris Hunting</h1>
+          <p className={styles.location}>Licking, MO</p>
+        </div>
+        <div className={styles.rightSection}>
+          <p className={styles.email}>harrishuntinglodge@gmail.com</p>
+          <div className={styles.weather}>
+            <span className={styles.weatherText}>
+              Current weather & time in Licking, MO:{" "}
+            </span>
+            <img
+              className={styles.weatherImg}
+              src={weatherIconSrc}
+              alt="Weather icon"
+            />
+            <span className={styles.temperature}>{temperature}</span>
+            <span className={styles.currentTime}>{currentTime}</span>
+          </div>
+
+          <div className={styles.navLinks}>
+            <Link to="/" className={styles.navLink}>
+              Home
+            </Link>
+            <Link to="/about" className={styles.navLink}>
+              About Us
+            </Link>
+            <Link to="/gallery" className={styles.navLink}>
+              Gallery
+            </Link>
+            <Link to="/contact" className={styles.navLink}>
+              Contact
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className={styles.rightSection}>
-        <p className={styles.email}>harrishuntinglodge@gmail.com</p>
-        <div className={styles.weather}>
+      {mobileNavOpen && (
+      <div className={`${styles.mobileNav} ${mobileNavOpen ? styles.mobileNavOpen : ''}`}>
+        <button className={styles.closeButton} onClick={() => setMobileNavOpen(false)}>X</button>
+        <Link to="/" className={styles.navLink}>Home</Link>
+        <Link to="/about" className={styles.navLink}>About Us</Link>
+        <Link to="/gallery" className={styles.navLink}>Gallery</Link>
+        <Link to="/contact" className={styles.navLink}>Contact</Link>
+      </div>
+    )}
+    <div className={styles.infoBar}>
+      
+      <div className={styles.weather}>
         <span className={styles.weatherText}>Current weather & time in Licking, MO: </span>
-          <img className={styles.weatherImg} src={weatherIconSrc} alt="Weather icon" />
-          <span className={styles.temperature}>{temperature}</span>
-          <span className={styles.currentTime}>{currentTime}</span>
-        </div>
-        
-        <div className={styles.navLinks}>
-          <Link to="/" className={styles.navLink}>Home</Link>
-          <Link to="/about" className={styles.navLink}>About Us</Link>
-          <Link to="/gallery" className={styles.navLink}>Gallery</Link>
-          <Link to="/contact" className={styles.navLink}>Contact</Link>
-        </div>
+        <img className={styles.weatherImg} src={weatherIconSrc} alt="Weather icon" />
+        <span className={styles.temperature}>{temperature}</span>
+        <span className={styles.currentTime}>{currentTime}</span>
       </div>
+      <p className={styles.email}>harrishuntinglodge@gmail.com</p>
     </div>
-  );
+  </div>
+);
 }
 
 export default Bannerr;
