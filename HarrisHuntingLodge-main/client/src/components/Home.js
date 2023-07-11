58cbfd1f6cd0c94e984a3ed7c955b04a562c8872
Rob Harris <rob.harris.rel@gmail.com>
2023-07-11 18:46:43 -0500

    Backtrack

--- HarrisHuntingLodge-main/client/src/components/Home.js
+++ HarrisHuntingLodge-main/client/src/components/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
 
-function Home() {
+const Home = () => {
   return (
-    <div className={styles.container}>
-      <h1>Welcome to Harris Hunting Lodge</h1>
-      <p>This is the home page.</p>
-    </div>
+    <div>
+      <h1>Home</h1>
+      </div>
+
+  
   );
-}
+};
 
 export default Home;
